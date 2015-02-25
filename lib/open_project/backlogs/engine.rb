5b55922bed91b60c138554127937d6986506303f
Alex Groleau <awgneo@xerxes0.teampalmtree.com>
2015-02-25 01:41:03 -0500

    progress

--- lib/open_project/backlogs/engine.rb
+++ lib/open_project/backlogs/engine.rb
@@ -9,42 +9,50 @@ module OpenProject::Backlogs
     include OpenProject::Plugins::ActsAsOpEngine
 
     register 'openproject-backlogs',
-             :author_url => 'http://finn.de',
-             :requires_openproject => '>= 3.0.0pre13' do
+    :author_url => 'http://finn.de',
+    :requires_openproject => '>= 3.0.0pre13' do
 
-	    project_module :backlogs do
+      project_module :backlogs do
 
-		    permission :view_backlogs, {
-		    	:backlogs => [:index]
-		    }
+        permission :view_backlogs, {
+        :backlogs => [:index]
+        }
 
-			menu :project_menu,
-			:backlogs,
-			{ :controller => '/backlogs', :action => :index },
-			:caption => 'Backlogs',
-			:after => :work_packages,
-			:param => :project_id,
-			:html => {:class => 'icon2 icon-backlogs-icon'}
+        menu :project_menu,
+        :backlogs,
+        { :controller => '/backlogs', :action => :index },
+        :caption => 'Backlogs',
+        :after => :work_packages,
+        :param => :project_id,
+        :html => {:class => 'icon2 icon-backlogs-icon'}
 
-			menu :project_menu,
-			:view_all_backlogs,
-            { controller: '/backlogs', :action => :index },
-            param: :project_id,
-            caption: 'View All',
-            parent: :backlogs,
-            html: { class: 'icon2 icon-backlogs-icon' }
+        menu :project_menu,
+        :view_all_backlogs,
+        { controller: '/backlogs', :action => :index },
+        param: :project_id,
+        caption: 'View All',
+        parent: :backlogs,
+        html: { class: 'icon2 icon-backlogs-icon' }
 
-            menu :project_menu,
-			:graphs,
-            { controller: '/backlogs', :action => :index },
-            param: :project_id,
-            caption: 'Graphss',
-            parent: :backlogs,
-            html: { class: 'icon2 icon-stats' }
+        menu :project_menu,
+        :graphs,
+        { controller: '/backlogs', :action => :index },
+        param: :project_id,
+        caption: 'Graphs',
+        parent: :backlogs,
+        html: { class: 'icon2 icon-stats' }
 
-		end
+        menu :project_menu,
+        :burndown,
+        { controller: '/backlogs', :action => :index },
+        param: :project_id,
+        caption: 'Burndown',
+        parent: :graphs,
+        html: { class: 'icon2 icon-stats' }
 
-	end
+      end
+
+    end
 
   end
 end
