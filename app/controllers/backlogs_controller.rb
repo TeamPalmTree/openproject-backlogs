0fa4ac3bf271ec2ca4e754d32050b3f9302d47c1
Alex Groleau <awgneo@xerxes0.teampalmtree.com>
2015-02-25 01:37:56 -0500

    progress

--- app/controllers/backlogs_controller.rb
+++ app/controllers/backlogs_controller.rb
@@ -3,6 +3,12 @@ class BacklogsController < ApplicationController
 
  	menu_item :backlogs
 
+ 	before_filter :load_project, :authorize
+
+ 	def load_project
+		@project = Project.find(params[:project_id])
+	end
+
 	def index
 	end
 
